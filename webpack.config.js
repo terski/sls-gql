c5d183434aa67550c8d99431df97b635082764bd
Matt Terski <matt.terski@gmail.com>
2018-10-14 22:40:06 -0500

    Work around webpack 4 build errors
    https://github.com/apollographql/react-apollo/issues/1737

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
     target: 'node',
     module: {
         rules: [
+            {
+                type: 'javascript/auto',
+                test: /\.mjs$/,
+                use: [],
+            },
             // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
             { test: /\.tsx?$/, loader: 'ts-loader' },
         ],
